f49e287e198ae0dc908a0ee0e4e57c1390dc6f38
Daria Gromova <igromova81@gmail.com>
2022-06-28 14:10:15 +0300

    Задания 1-4. Тема 1.5 Массивы часть1. Исправленное

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,87 @@
 public class Main {
     public static void main(String[] args) {
-        task1_2();
-    }
-    // Задание 1, 2
-    public static void task1_2() {
+        // Задание 1, 2
         // Массив 1
         byte[] arr = new byte[3];
-        arr [0] = 1;
-        arr [1] = 2;
-        arr [2] = 3;
-        for (int i = 0; i < arr.length-1; i++) {
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = 0; i < arr.length - 1; i++) {
             System.out.print(arr[i] + ", ");
         }
-        System.out.print(arr[2]);
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr[i]);
+            }
+        }
         System.out.println();
         // Массив 2
         float[] box = {1.57F, 7.654F, 9.986F};
-        for (int c = 0; c < box.length-1; c++) {
-            System.out.print(box[c] + ", ");
+        for (int i = 0; i < box.length - 1; i++) {
+            System.out.print(box[i] + ", ");
         }
-        System.out.print(box[2]);
+        for (int i = 0; i < box.length; i++) {
+            if (i == box.length - 1) {
+                System.out.print(box[i]);
+            }
+        }
         System.out.println();
         // Массив 3
         int[] bag = new int[]{5, 6, 33, 7, 6};
-        for (int x = 0; x < bag.length-1; x++) {
-            System.out.print(bag[x] + ", ");
+        for (int i = 0; i < bag.length - 1; i++) {
+            System.out.print(bag[i] + ", ");
         }
-        System.out.print(bag[4]);
+        for (int i = 0; i < bag.length; i++) {
+            if (i == bag.length - 1) {
+                System.out.print(bag[i]);
+            }
+        }
         System.out.println();
         // Задание 3
         // Массив 1
-        for (int i = 2; i > 0; i--) {
+        for (int i = arr.length - 1; i < arr.length && i > 0; i--) {
             System.out.print(arr[i] + ", ");
         }
-        System.out.print(arr[0]);
+        for (int i = 0; i < arr.length; i--) {
+            if (i == 0) {
+                System.out.print(arr[i]);
+            }
+        }
         System.out.println();
         // Массив 2
-        for (int c = 2; c > 0; c--) {
-            System.out.print(box[c] + ", ");
+        for (int i = box.length - 1; i < box.length && i > 0; i--) {
+            System.out.print(box[i] + ", ");
         }
-        System.out.print(box[0]);
+        for (int i = 0; i < box.length; i--) {
+            if (i == 0) {
+                System.out.print(box[i]);
+            }
+        }
         System.out.println();
         // Массив 3
-        for (int x = 4; x > 0; x--) {
-            System.out.print(bag[x] + ", ");
+        for (int i = bag.length - 1; i < bag.length && i > 0; i--) {
+            System.out.print(bag[i] + ", ");
         }
-        System.out.print(bag[0]);
+        for (int i = 0; i < bag.length; i--) {
+            if (i == 0) {
+                System.out.print(bag[i]);
+            }
+        }
         System.out.println();
         // Задание 4
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i] % 2 == 0 && arr[i] != 0) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] % 2 == 0) {
                 System.out.print(arr[i] + ", ");
-            } else if (arr[i] % 2 != 0 && arr[i] != 0) {
+            } else if (arr[i] % 2 != 0) {
                 System.out.print(arr[i] + 1 + ", ");
             }
         }
-        if (arr[2] % 2 != 0 && arr[2] != 0) {
-            System.out.print(arr[2] + 1);
-        } else {
-            System.out.print(arr[2]);
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1 && arr[i] % 2 == 0) {
+                System.out.print(arr[i]);
+            } else if (i == arr.length - 1 && arr[i] % 2 != 0) {
+                System.out.print(arr[i] + 1);
+            }
         }
     }
 }
